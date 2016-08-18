4baf4e03faa65f5b7654039d5caf5bdbc9a905b2
Ttiamus <ttiamus@gmail.com>
2016-08-18 10:28:03 -0500

    forgot to update my item definition in component

--- src/app/items/items-detail.component.ts
+++ src/app/items/items-detail.component.ts
@@ -15,10 +15,10 @@ export class ItemsDetailComponent implements OnInit {
 
     ngOnInit() { 
      this.generalItems = [
-       { 'id': 11, 'name': 'Chewbacca' },
-       { 'id': 12, 'name': 'Rey' },
-       { 'id': 13, 'name': 'Finn (FN2187)' },
-       { 'id': 14, 'name': 'Han Solo' },
-       { 'id': 15, 'name': 'Leia Organa' }
+       { 'id': 11, 'name': 'Chewbacca', description:'hairy', type:'wookie', cost:1, weight:1 },
+       { 'id': 12, 'name': 'Rey', description:'human', type:'woman', cost:1, weight:1 },
+       { 'id': 13, 'name': 'Finn (FN2187)', description:'traitor', type:'clone trooper', cost:1, weight:1 },
+       { 'id': 14, 'name': 'Han Solo', description:'shot first', type:'wookie', cost:1, weight:1 },
+       { 'id': 15, 'name': 'Leia Organa', description:'wookie', type:'wookie', cost:1, weight:1 }
      ];}
 }
